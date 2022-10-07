73d1d8696a00372059a16a62569fc39a49e0db4f
brisa briceño <bricenobrisa739@gmail.com>
2022-10-06 22:00:04 -0300

    muestro en pantalla las variables

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ var numerotel = '2995345673';
 let escuela = `epet 20`;
 var color = 'blanco';
 //#endregion
+//#region console.log
+console.log(nombre);
+console.log(apellido);
+console.log(numerotel);
+console.log(escuela);
+console.log(color);
+//#endregion
